0467ef576daa35f285eb6a20f6f3e1195ef061e7
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-17 22:40:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  if number == 2 || number == 3
+    return true
+  elsif number <= 1
+    return false
+  else
+    range = (2..number).to_a
+    for i in range
+      if number % i != 0
+        return true
+      else
+        return false
+      end
+    end
+  end
+end
